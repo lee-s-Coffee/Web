2d699f71a2e2f0837d7b56300bbcc4b0bd6152f8
luda412 <201810791@sangmyung.kr>
2024-09-14 10:16:49 +0900

    [feat] 데이터 패치를 위한 api 프록시 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: "http://localhost:8080/:path*",
+      },
+    ];
+  },
+
+  env: {
+    NEXT_PUBLIC_NEXT_APP_BASE_URL: process.env.NEXT_APP_BASE_URL,
+    NEXT_PUBLIC_NEXT_APP_DOMAIN_URL: process.env.NEXT_APP_DOMAIN_URL,
+  },
+};
 
 export default nextConfig;
