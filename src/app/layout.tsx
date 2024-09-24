4f9e24cfe65c89bc1cf02bcbd8ad7436e14de713
luda412 <201810791@sangmyung.kr>
2024-09-24 15:46:35 +0900

    [Setting] - 메타 데이터 및 아이콘 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,10 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Lee's Coffee",
-  description: "Lee's Coffee Web Site, ",
+  description: "Lee's Coffee Web Site",
+  icons: {
+    icon: "/favicon.ico", // 경로 설정
+  },
 };
 
 export default function RootLayout({
